b460c8df29fe3383f9d35acae26ec1ff5cc0b12c
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2022-11-21 12:26:55 +0200

    Update palindromeChecker.js

--- palindromeChecker.js
+++ palindromeChecker.js
@@ -2,3 +2,13 @@ function isPalindrome(string) {
     let reversed = string.split("").reverse().join("");
     return string === reversed
 }
+
+function isPalindrome(string) {
+    const length = string.length
+    for (let i = 0; i < length / 2; i++) {
+        if (string[i] !== string[length - 1 - i]) {
+            return false
+        }
+    }
+    return true
+}
