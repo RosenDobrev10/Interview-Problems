e27e910686395957a95e9a2e7fde062fece880fb
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2022-11-22 10:09:48 +0200

    Update bubbleSort.js

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,12 +1,32 @@
+// Standart bubble sort
+function bubbleSort(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    return arr;
+}
+
+// bubble sort stops, when there are no swap 
 function bubbleSort(arr) {
 
     for (let i = 0; i < arr.length; i++) {
+        let isSwapped = false
         for (let j = 0; j < arr.length - 1; j++) {
             if (arr[j] > arr[j + 1]) {
                 let temp = arr[j]
                 arr[j] = arr[j + 1]
                 arr[j + 1] = temp
+                isSwapped = true
             }
+        }
+        if (!isSwapped) {
+            return arr
         }
     }
     return arr
